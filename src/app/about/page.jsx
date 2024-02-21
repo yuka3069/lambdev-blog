5a3616638bd9eba5a52f4fd0b1d7813aae30de0e
LAPTOP-0HPUCQM3\Yunyu <yunyuancheng@outlook.com>
2024-02-21 19:56:15 +0800

    complete about page part

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,5 +1,37 @@
+import styles from "./about.module.css";
+import Image from "next/image";
+
 const AboutPage = () => {
-  return <div>this is an about page!</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h2 className={styles.subTitle}>About Agency</h2>
+        <h1 className={styles.title}>this is a title</h1>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus,
+          quibusdam.
+        </p>
+        <div className={styles.boxes}>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Year of experience</p>
+          </div>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Year of experience</p>
+          </div>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Year of experience</p>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.imgContainer}>
+        <Image src="/about.png" alt="" fill className={styles.img} />
+      </div>
+    </div>
+  );
 };
 
 export default AboutPage;
